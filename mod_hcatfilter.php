3e70d351deac0743cf921012f90896bd002ad6e4
Fedik <getthesite@gmail.com>
2015-04-05 16:34:25 +0300

    Preload selected

--- mod_hcatfilter.php
+++ mod_hcatfilter.php
@@ -14,18 +14,16 @@ defined('_JEXEC') or die('Get lost?');
 // Include the syndicate functions only once
 require_once dirname(__FILE__) . '/helper.php';
 
-//already defined in the joomla application module helper:
-//$module, $attribs, $app, $params, $scope, $path, $chrome, $lang
-
 $app  = JFactory::getApplication();
 $doc  = JFactory::getDocument();
 $user = JFactory::getUser();
-//container for js options
-$options = array();
 
 //get settings
-$class_sfx	= htmlspecialchars($params->get('class_sfx'));
-$use_ajax = $params->get('use_ajax', 0);
+$class_sfx = htmlspecialchars($params->get('class_sfx'));
+$block_id  = 'mod-hcatfilter-' . $module->id;
+$use_ajax  = $params->get('use_ajax', 0);
+$Itemid    = $params->get('assign_menu') ? $params->get('menu_item') : $app->input->getInt('Itemid');
+$options   = array();
 
 //check whether ajax call
 $is_ajax = !empty($module->ajax) && $use_ajax;
@@ -37,23 +35,27 @@ $root_catid = $root_catid == '0' ? '1' : $root_catid;
 //for AJAX use sended id instead of selected in configuration
 $root_catid = !$is_ajax ? $root_catid : $app->input->get('id', '', 'string');
 
-//use caching
+//load categories, use caching
 $cache = JFactory::getCache('mod_hcatfilter', 'callback');
-//load categories
 $categories = $cache->call( array( 'modHcatFilterHelper', 'getCategories' ), $params, $user->getAuthorisedViewLevels());
 
-if (empty($categories) || empty($categories[$root_catid])) {
+if (empty($categories) || empty($categories[$root_catid]))
+{
 	echo JText::_('JLIB_DATABASE_ERROR_EMPTY_ROW_RETURNED');
 	return;
 }
 
 //get categories sorted by their parents
-if (!$use_ajax) {
+if (!$use_ajax)
+{
 	$cat_tree = $cache->call( array( 'modHcatFilterHelper', 'getCatsFullTree' ), $categories);
-} else {
+}
+else
+{
 	// @TODO: load also preselected
 	$cat_tree = array($root_catid => modHcatFilterHelper::getCatsForOneLevel($categories[$root_catid]->children));
-	if ($is_ajax) {
+	if ($is_ajax)
+	{
 		echo json_encode($cat_tree[$root_catid]);
 		return;
 	}
@@ -64,22 +66,33 @@ if (!$use_ajax) {
 
 //curent category id
 $active_catid = 0;
-if ($app->input->get('option') == 'com_content' && $app->input->get('view') == 'category') {
+if ($app->input->get('option') == 'com_content' && $app->input->get('view') == 'category')
+{
 	$active_catid = $app->input->get('id', 0, 'string');
 }
-elseif ($app->input->get('option') == 'com_content' && $app->input->get('view') == 'article') {
+elseif ($app->input->get('option') == 'com_content' && $app->input->get('view') == 'article')
+{
 	$active_catid = $app->input->get('catid', 0, 'string');
 }
 
 $options['preselect'] = ($active_catid) ? modHcatFilterHelper::getActivePath($categories, $active_catid) : array();
+if($use_ajax)
+{
+	// Preload selected
+	foreach($options['preselect'] as $s){
+		if($s == $root_catid)
+		{
+			// It alredy loaded
+			continue;
+		}
+		$cat_tree[$s] = modHcatFilterHelper::getCatsForOneLevel($categories[$s]->children);
+	}
+}
 
-//for assign result for menu item
-$Itemid = $params->get('assign_menu') ? $params->get('menu_item') : $app->input->getInt('Itemid');
-
-$block_id = 'mod-hcatfilter-' . $module->id;
 //get labels
 $labels = trim(htmlspecialchars($params->get('labels'), ENT_QUOTES));
-if($labels){
+if ($labels)
+{
 	$options['labels'] = explode(',', $labels);
 }
 
